e48a20ba32786151acc8db2e5529b474528d778b
melisa taskara <taskaramelisa@gmail.com>
2025-09-24 18:25:19 +0300

    projenin ilk dosyaları eklendi

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("login-button").onclick = function () {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Basit kontrol (şimdilik DB yok)
+    if() {
+      //DB ile kayıtlar eslesmis ve giris basarılıysa
+    } else {
+        //DB de kayıt bulunamamıs eslesemmeis ise
+    }
+};
